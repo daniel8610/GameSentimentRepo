748e267523c5ed8901d933df016959f8cd944fd9
daniel8610 <daniel1086@hotmail.it>
2015-09-14 20:31:56 +0200

    cleaner URL

--- GameSentiment/src/main/java/GameSentiment/Utility/TweetCleaner.java
+++ GameSentiment/src/main/java/GameSentiment/Utility/TweetCleaner.java
@@ -1,10 +1,19 @@
 package GameSentiment.Utility;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.nibor.autolink.LinkExtractor;
+import org.nibor.autolink.LinkSpan;
+
 public class TweetCleaner {
 	
 	//regex rimuovi un qualsiasi stringa che inizia con # e contiene 0 (*)o piu lettere presenti in [A-Za-z] o ( | )....
 	public static String cleanTweet(String text){
-		return text.replaceAll("#[A-Za-z]*|@[A-Za-z]*", "");
+		String temp;
+		temp=text.replaceAll("((https?|ftp|gopher|telnet|file|Unsure|http):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)", "");
+		
+		return temp.replaceAll("#[A-Za-z]*|@[A-Za-z]*", "");
 	}
 
 }
