e10de91661f8764d8d7b6ee320ade2a2d37b21f6
daniel8610 <daniel1086@hotmail.it>
2015-09-17 19:09:25 +0200

    urlexpander

--- GameSentiment/src/main/java/GameSentiment/GameSentiment/main.java
+++ GameSentiment/src/main/java/GameSentiment/GameSentiment/main.java
@@ -8,6 +8,7 @@ import org.nibor.autolink.LinkExtractor;
 import org.nibor.autolink.LinkSpan;
 
 import GameSentiment.Utility.HtmlCleaner;
+import GameSentiment.Utility.TweetCleaner;
 import GameSentiment.Utility.URLextractor;
 
 
@@ -15,7 +16,7 @@ public class main
 {
     public static void main( String[] args )
     {
-    	String topic="Metal gear graphics";
+    	/*String topic="Metal gear graphics";
     	LinkedList<Tweet> output=new LinkedList<Tweet>();
     	LinkedList<String> entita=null;
     	ArrayList<String> tweets=TweetManager.getTweets(topic);
@@ -45,10 +46,10 @@ public class main
     			e.printStackTrace();
     		}
         
-        	
-   
+        }	
+   */
     	
-        }
+        
         
         
         
@@ -62,9 +63,11 @@ public class main
 			e.printStackTrace();
 		}
 		*/
+    	String f="I liked a @YouTube http://t.co/tSphXPehfR video from @candylandgs http://t.co/tSphXPehfR Metal Gear Solid 5 The Phantom Pain – PS3 vs. PS4 Graphics http://t.co/tSphXPehfR";
     	
-    	//WrapperStanfordNLP.init();
-    	//System.out.println(WrapperStanfordNLP.findSentiment2("Even with a slightly larger screen, the iPhone remains comfortable to hold. With fast performance, a great display, an elegant new design and a much-needed software update, it's one of the best smartphones you can buy right now. We wish it had the same long battery life as the iPhone 6 Plus (not to mention its optical image stabilization) but even then, the iPhone 6 is still the better choice for most people."));
+    	System.out.println(TweetCleaner.cleanTweet(f));
+    	WrapperSentimentStanford.init();
+    	System.out.println(WrapperSentimentStanford.findSentiment2("I liked a  video from  Metal Gear Solid 5 The Phantom Pain – PS3 vs. PS4 Graphics"));
         
-    
+}
 }
